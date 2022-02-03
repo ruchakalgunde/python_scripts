99525ed7d757b9ce3c8b4fefeef4e70d82e1603a
ruchakalgunde <68113835+ruchakalgunde@users.noreply.github.com>
2022-02-03 13:52:26 +0530

    Create json_url_accessible_tool.py

---
+++ json_url_accessible_tool.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import requests
+import sys
+import argparse
+import os
+import subprocess as sp
+
+parser = argparse.ArgumentParser(description="Check json URL accessible tool")
+parser.add_argument("-apk", type=str, help="Enter apk file", required=True)
+
+a = parser.parse_args()
+#h = sys.argv[1]+"/.json"
+h = sp.getoutput("strings {} | egrep -o https\:\/\/\(.*\).firebaseio.com ".format(a.apk))
+url = ("{}/.json".format(h))
+print (h)
+r=requests.get(url)
+if r.status_code == 200:
+    print("Readable")
+else:
+    print("Non-Redable")
+
+
+payload={'text':'anything'}
+r=requests.put(h,data=payload)
+if r.status_code == 201 or r.status_code == 200:
+    print("Writable")
+else:
+    print("Can't write")
